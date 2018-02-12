af2fef9d2812b588d5330bf40e99aedbae82f8d0
James Kingston <james.kingston@digital.dvla.gov.uk>
2018-02-12 12:24:29 +0000

    param added

--- rpg-location-service/pipeline.groovy
+++ rpg-location-service/pipeline.groovy
@@ -1,9 +1,14 @@
 pipeline {
     agent any
+    parameters {
+      stringParam(defaultValue: 'latest', description: '', name: 'dockerTag')
+    }
+
     stages {
         stage('Deploy') {
             steps {
-                sh 'echo deploy job started'
+                sh 'echo deploy job started '
+                sh 'echo ${params.dockerTag}'
             }
         }
     }
