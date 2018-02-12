4d1c98e67fe4cf632ef34bb133d1eec32f7d2d06
James Kingston <james.kingston@digital.dvla.gov.uk>
2018-02-12 15:43:39 +0000

    var already declared

--- rpg-location-service/pipeline.groovy
+++ rpg-location-service/pipeline.groovy
@@ -23,7 +23,7 @@ pipeline {
         stage('Update the task definition') {
             steps {
               script{
-                def update_retval = sh script:"${env.WORKING_DIR}/update-task-def.sh ${params.dockerTag} ${params.environment}", returnStdout: true
+                update_retval = sh script:"${env.WORKING_DIR}/update-task-def.sh ${params.dockerTag} ${params.environment}", returnStdout: true
               }
             }
         }
