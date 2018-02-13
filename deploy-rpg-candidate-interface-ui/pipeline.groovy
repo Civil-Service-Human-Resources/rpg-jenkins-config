397be037556cf1e79a901515843c0a503f3a5fbc
James Kingston <james.kingston@digital.dvla.gov.uk>
2018-02-13 12:16:29 +0000

    beginning of candidate interface pipeline

---
+++ deploy-rpg-candidate-interface-ui/pipeline.groovy
@@ -0,0 +1,24 @@
+def update_retval = 'NA'
+
+pipeline {
+    agent any
+    environment {
+      WORKING_DIR='${WORKSPACE}'+'/deploy-rpg-candidate-interface-ui'
+      AWS_DEFAULT_REGION='eu-west-1'
+    }
+    parameters {
+      string(defaultValue: 'latest', description: '', name: 'dockerTag')
+      string(defaultValue: 'dev', description: '', name: 'environment')
+    }
+
+    stages {
+        stage('Check parameters') {
+            steps {
+                sh "echo deploy job started "
+                sh "echo dockerTag: ${params.dockerTag}"
+                sh "echo environmentTag: ${params.environment}"
+            }
+        }
+
+    }
+}
