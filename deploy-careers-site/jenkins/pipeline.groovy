7502900aace4839832cc6a4dcee8069810939849
James Kingston <james.kingston@valtech.co.uk>
2018-02-26 12:48:05 +0000

    addition of pipeline steps

--- deploy-careers-site/jenkins/pipeline.groovy
+++ deploy-careers-site/jenkins/pipeline.groovy
@@ -7,6 +7,7 @@ pipeline {
     parameters {
       string(defaultValue: 'release', description: '', name: 'branch_name')
       string(defaultValue: 'jenkins', description: '', name: 'built_by')
+      string(defaultValue: '', description: '', name: 'credentials_id')
     }
 
     stages {
@@ -18,9 +19,20 @@ pipeline {
             }
         }
 
+        stage('Git clone the branch') {
+          steps {
+            sh "mkdir -p ${WORKING_DIR}/ansible/source"
+            git(
+               url: 'git@github.com:Civil-Service-Human-Resources/rpg-careers-wp.git',
+               credentialsId: '${params.credentials_id}',
+               branch: '${params.branch_name}'
+            )
+          }
+        }
+
         stage('Build deployment zip') {
             steps {
-                sh "ansible-playbook ${WORKING_DIR}/ansible/package.yml --extra-vars 'user=${params.built_by}'"
+                sh "ansible-playbook ${WORKING_DIR}/ansible/package.yml --extra-vars 'user=${params.built_by}' --skip-tags 'git_checkout'"
             }
         }
     }
