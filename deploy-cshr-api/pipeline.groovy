9b322ad00c2e71e3fc38139cedeabdb4ccfd0658
James Kingston <james.kingston@digital.dvla.gov.uk>
2018-03-13 16:44:26 +0000

    addition of API update and common script.

--- deploy-cshr-api/pipeline.groovy
+++ deploy-cshr-api/pipeline.groovy
@@ -4,11 +4,14 @@ pipeline {
     agent any
     environment {
       WORKING_DIR='${WORKSPACE}'+'/deploy-cshr-api'
+      COMMON_DIR='${WORKSPACE}'+'/common'
       AWS_DEFAULT_REGION='eu-west-1'
     }
     parameters {
-      string(defaultValue: 'latest', description: '', name: 'dockerTag')
+      string(defaultValue: 'latest', description: 'The circle CI tag', name: 'dockerTag')
       string(defaultValue: 'dev', description: '', name: 'environment')
+      //string(description: 'CSHR database user name for the API', name: 'datasource_username')
+      //string(description: 'CSHR database password for the API', name: 'datasource_password')
     }
 
     stages {
@@ -20,5 +23,33 @@ pipeline {
             }
         }
 
+        stage('Update the task definition') {
+            steps {
+              withCredentials([usernamePassword(credentialsId: '${params.environment}-db_root', usernameVariable: 'user', passwordVariable: 'pass' )]
+                script{
+                  update_retval = sh script:"${env.WORKING_DIR}/update-task-def.sh ${params.dockerTag} ${params.environment} ${user} ${pass}", returnStdout: true
+                }
+            }
+        }
+
+        stage('Show results.') {
+            steps {
+                echo "${update_retval}"
+            }
+        }
+
+        stage('Update the service') {
+            steps {
+              script {
+                def update_output = readJSON text: "${update_retval}"
+                def arn = update_output['taskDefinition']['taskDefinitionArn']
+                sh "${env.COMMON_DIR}/update-service.sh ${arn} ${params.environment} candidate-interface-api"
+              }
+            }
+        }
+
+
     }
+
+
 }
---
+++ deploy-cshr-api/update-task-def.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Create the container definition
+DOCKER_TAG=${1}
+DEPLOY_ENV=${2}
+datasource_username=${3}
+datasource_password=${4}
+
+
+CONTAINER_DEFINITION=$(cat <<EOF
+  {
+    "name": "cshr-api",
+    "image": "cshr-docker-rpg.bintray.io/cshr-api:${DOCKER_TAG}",
+    "memory": 512,
+    "essential": true,
+    "hostname": "api",
+    "portMappings": [
+        {
+            "containerPort": 8080,
+            "hostPort": 8080
+        }
+    ],
+    "environment": [
+      {
+        "name": "LOCATION_SERVICE_URL",
+        "value": "https://location-service.${DEPLOY_ENV}-internal.cshr-gov.uk/findlocation/{searchTerm}"
+      },
+      {
+        "name": "DATASOURCE_URL",
+        "value": "jdbc:postgresql://candidate-interface-db.${DEPLOY_ENV}.cshr-gov.uk:5432/cshr"
+      },
+      {
+        "name": "DATASOURCE_USERNAME",
+        "value": "${datasource_username}"
+      },
+      {
+        "name": "DATASOURCE_PASSWORD",
+        "value": "${datasource_password}"
+      }
+
+    ]
+  }
+
+EOF
+)
+
+aws ecs register-task-definition --family ${DEPLOY_ENV}-candidate-interface-ui --container-definitions "${CONTAINER_DEFINITION}"
