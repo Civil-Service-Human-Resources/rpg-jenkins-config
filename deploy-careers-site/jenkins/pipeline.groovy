0c18c47eec121f095d6a9361f46032d36bc7f0fb
James Kingston <james.kingston@i32.co.uk>
2018-06-01 12:25:15 +0100

    updated base ref

--- deploy-careers-site/jenkins/pipeline.groovy
+++ deploy-careers-site/jenkins/pipeline.groovy
@@ -41,7 +41,7 @@ pipeline {
             steps {
                 parallel(
                     package: {
-                        sh "ansible-playbook ./deploy-careers-site/ansible/package.yml --extra-vars \"user=${params.built_by}\" --extra-vars \"base_dir=${WORKSPACE}\""
+                        sh "ansible-playbook ./deploy-careers-site/ansible/package.yml --extra-vars \"user=${params.built_by}\" --extra-vars \"base_dir=${BASE_DIR}\""
                     },
                     basic_install: {
                         withCredentials([usernamePassword(credentialsId: "${params.environment}_db_root", usernameVariable: 'user', passwordVariable: 'pass' )]){
