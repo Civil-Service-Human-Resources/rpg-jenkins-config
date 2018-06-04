28c6bb65fe1c4944fa416e57b75b887829de57f3
James Kingston <james.kingston@digital.dvla.gov.uk>
2018-06-04 15:28:37 +0100

    change to ansible script to standardise zip

--- deploy-careers-site/jenkins/pipeline.groovy
+++ deploy-careers-site/jenkins/pipeline.groovy
@@ -64,15 +64,6 @@ pipeline {
 
         stage('deploy the zip file') {
             steps {
-              //script{
-              //  zip_file_name = sh script:"ls -1rt ${env.WORKING_DIR}/ansible/zip | tail -1 | sed 's/\\n//g'", returnStdout: true
-              //}
-              script {
-                sh '''
-                  file_name=$(ls -1rt ${env.WORKING_DIR}/ansible/zip | tail -1)
-                  cp ${file_name} ${env.WORKING_DIR}/ansible/zip/jenkins_deployment.zip
-                '''
-              }
               withCredentials([usernamePassword(credentialsId: "${params.environment}_db_root", usernameVariable: 'user', passwordVariable: 'pass' )]){
                 wrap([$class: 'AnsiColorBuildWrapper', 'colorMapName': 'XTerm']) {
                   ansiblePlaybook(
